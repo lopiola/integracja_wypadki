c9ffc95971e7c7cdb5ffbee0c9d48745be4fa82c
Łukasz Opioła <lukasz.opiola@gmail.com>
2015-04-27 14:27:50 +0200

    add more scripts

--- scripts/db_api/create_tables.py
+++ scripts/db_api/create_tables.py
@@ -9,6 +9,7 @@ from psycopg2 import connect
 
 import common
 import accident
+import vehicle
 
 
 user = common.get_user()
@@ -19,6 +20,7 @@ con = connect(user=user, database=database)
 cur = con.cursor()
 
 cur.execute(accident.create_table_command())
+cur.execute(vehicle.create_table_command())
 
 cur.close()
 con.commit()
