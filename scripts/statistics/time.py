884f34f0f8381a28c8c9c89cbcbdc34e9a4b6731
Beata Skiba <bskiba@student.agh.edu.pl>
2015-05-17 17:05:21 +0200

    More time statistics

--- scripts/statistics/time.py
+++ scripts/statistics/time.py
@@ -16,6 +16,9 @@ def monthly_statistics():
 def time_statistics(time_period='year'):
     return accident.execute_query(time_statistics_query().format(time_period=time_period))
 
+def daily_statistics():
+    return accident.execute_query(daily_statistics_query())
+
 
 def time_statistics_query():
     return '''
@@ -24,10 +27,20 @@ group by date_part('{time_period}', timestamp)
 order by date_part('{time_period}', timestamp);
 '''
 
+def daily_statistics_query():
+    return '''
+select date_part('day', timestamp) as day, date_part('month', timestamp) as month, count(*) as count from accident
+group by month, day
+order by count desc
+limit 10;
+'''
 
 if __name__ == '__main__':
-    for (year, count) in yearly_statistics():
-        print int(year), count
+    # for (year, count) in yearly_statistics():
+    #     print int(year), count
 
     # for (month, count) in monthly_statistics():
     #     print int(month), count
+
+    for (day, month, count) in daily_statistics():
+        print int(day), int(month), count
